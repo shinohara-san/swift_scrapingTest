891ad29e83269ac0e42299931e9ee73e8dddecfe
shinohara-san <someiyoshino0809@gmail.com>
2020-07-10 17:38:25 +0900

    first

---
+++ scrapingPractice/DetailViewController.swift
@@ -0,0 +1,45 @@
+//
+//  DetailViewController.swift
+//  scrapingPractice
+//
+//  Created by Yuki Shinohara on 2020/07/10.
+//  Copyright © 2020 Yuki Shinohara. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    var mentor: Mentor!
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var comment: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.async {[weak self] in
+            self?.nameLabel.text = self?.mentor.name
+            let image:UIImage = UIImage(url: self?.mentor.imageURL ?? "")
+            self?.imageView.image = image
+            self?.comment.text = self?.mentor.lead
+        }
+    }
+
+}
+
+extension UIImage {
+    public convenience init(url: String) {
+        let url = URL(string: "https://www.aeonet.co.jp" + url)
+        do {
+            print(url!)//urlが間違ってる可能性あるのでちゃんとアクセスできるかブラウザ確認
+            let data = try Data(contentsOf: url!)
+            self.init(data: data)!
+            return
+        } catch let err {
+            print("Error : \(err.localizedDescription)")
+        }
+        self.init()
+    }
+}
--- scrapingPractice/ViewController.swift
+++ scrapingPractice/ViewController.swift
@@ -7,14 +7,99 @@
 //
 
 import UIKit
+import Alamofire
+import Kanna
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var mentors = [Mentor]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
+        DispatchQueue.main.async { [weak self] in
+            self?.getData()
+        }
+        
     }
-
-
+    
+    func getData(){
+        AF.request("https://www.aeonet.co.jp/school/kyushu/fukuoka/4021/staff/").responseString { [weak self] (response) in
+            if let html = response.value {
+//                print(html)
+                var names = [String]()
+                if let doc = try? HTML(html: html, encoding: String.Encoding.utf8){
+                    for mentorName in doc.xpath("//h4[@class='teacher-name']"){
+                        names.append(mentorName.text ?? "")
+                    }
+//                print(names)
+                var urls = [String]()
+                if let doc = try? HTML(html: html, encoding: String.Encoding.utf8){
+                    let path = "//div[@class='teacher-image']/img/@src"
+//                    let path = "//p[@class='teacher-lead']"
+                 for urlName in doc.xpath(path){
+//                    print(urlName.text ?? "無し")
+                     urls.append(urlName.text ?? "無し")
+                 }
+                    
+                var comments = [String]()
+                    if let doc = try? HTML(html: html, encoding: String.Encoding.utf8){
+                        let path = "//p[@class='teacher-lead']"
+                        //                    let path = "//p[@class='teacher-lead']"
+                        for comment in doc.xpath(path){
+                            //                    print(urlName.text ?? "無し")
+                            comments.append(comment.text ?? "無し")
+                        }
+                    }
+                    for (index, value) in names.enumerated(){
+                        let mentor = Mentor()
+                        mentor.name = value
+                        mentor.imageURL = urls[index]
+                        mentor.lead = comments[index]
+                        self?.mentors.append(mentor)
+                    }
+                    
+                    self?.tableView.reloadData()
+                }
+            }
+        }
+    }
+}
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return mentors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = mentors[indexPath.row].name
+        cell.detailTextLabel?.text = mentors[indexPath.row].imageURL
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        let vc = storyboard?.instantiateViewController(identifier: "detailVC") as! DetailViewController
+        vc.mentor = mentors[indexPath.row]
+        self.navigationController?.pushViewController(vc, animated: true)
+    }
+    
+}
+
+class Mentor {
+    var name:String = ""
+    var imageURL: String = ""
+    var lead: String = ""
+//    init(name: String, imageURL:String) {
+//        self.name = name
+//        self.imageURL = imageURL
+//    }
+}
+
+//xpath
+// タグはそのまま、階層を降りる時はスラッシュ、タグ内にアクセスは@〇〇
